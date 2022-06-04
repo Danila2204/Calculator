7ec63526840f40ca2e177056dbac32aab1058212
Данила Гаврютенков <pogranichnik.marshal@yandex.ru>
2022-06-04 18:59:18 +0300

    Alpha-версия калькулятора 2.0 .

--- app.js
+++ app.js
@@ -1,8 +1,39 @@
 const conclusion = document.querySelector(".conclusion");
 const numbers = document.querySelectorAll(".number");
+const signs = document.querySelectorAll(".sign");
+
+let dot = false;
 
 for (let i = 0; i < numbers.length; i++) {
     numbers[i].addEventListener("click", () => {
-        conclusion.innerHTML += numbers[i].textContent;
+        if (numbers[i].textContent === ".") {
+            let number = [...numbers[i].textContent];
+
+            for (let i = 0; i < number.length; i++) {
+                if (number[i] === ".") {
+                    if (dot === false) {
+                        conclusion.textContent += ".";
+                        dot = true;
+                        break;
+                    }
+                }
+            }
+            
+            console.log("Цикл пройден!")
+
+        } else {
+            const firstNumber = numbers[i].textContent;
+            conclusion.textContent += firstNumber;
+        }
     });
+}
+
+for (let i = 0; i < signs.length; i++) {
+    signs[i].addEventListener("click", () => {
+        if (signs[i].textContent === "=") {
+        } else if (signs[i].textContent === "+") {
+        } else if (signs[i].textContent === "-") {
+        } else if (signs[i].textContent === "*") {
+        } else if (signs[i].textContent === "/") {}
+    })
 }
